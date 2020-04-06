0109a0762f261a265e666a2571b381484ad2b960
jason.moore@dbca.wa.gov.au <Jason Moore>
2020-04-06 18:40:01 +0800

    Installer Files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='django_crispy_jcaptcha',
+      version='1.1',
+      description='Django Crispy Form Image Captcha',
+      url='https://github.com/xzzy/django_crispy_jcaptcha',
+      author='Jason Moore',
+      author_email='jason@digitalreach.com.au',
+      license='BSD',
+      packages=['migrations', ],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
+
+
